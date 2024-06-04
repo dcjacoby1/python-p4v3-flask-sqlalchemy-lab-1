50246d9dd680721b96194221c9ca6c868749a24d
Dan <jacobydan01@gmail.com>
2024-06-04 14:55:43 -0400

    views, filters, Db init, defining models

--- server/app.py
+++ server/app.py
@@ -21,7 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
 
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    if earthquakes:
+        quakes = [earthquake.to_dict() for earthquake in earthquakes]
+        body = {
+            'count': len(quakes),
+            'quakes': quakes
+        }
+        status = 200
+    else:
+        body = {
+            'count': 0,
+            'quakes': []
+        }
+        status = 200
+    
+    return make_response(body, status)
+    
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
